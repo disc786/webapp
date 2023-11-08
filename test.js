c81ce47b6508cf0264698a774542a6cffaa607c1
disc786 <148037505+disc786@users.noreply.github.com>
2023-11-08 10:17:34 +0300

    Update test.js

--- test.js
+++ test.js
@@ -14,36 +14,24 @@ function test(){
 }
 test();
 
-function loadHTMLContent(username) {
-const url = ''+ htmlFileURL;
-// Fetch HTML content from the specified URL
-fetch(url)
-  .then(response => {
-    // Check if the response is successful (status code 200)
-    if (response.ok) {
-      // If the response is successful, convert it to text
-      return response.text();
-    }
-    // If the response is not successful, throw an error
-    throw new Error('Network response was not ok.');
-  })
-  .then(html => {
-    // Store the response result in a variable
-    const responseResult = html;
-    
-    // Store the HTML result in a variable
-    const htmlResult = html;
-    
-    // You can now use the responseResult and htmlResult variables for further processing
-    console.log('Response Result:', responseResult);
-    console.log('HTML Result:', htmlResult);
-  })
-  .catch(error => {
-    // Handle errors that occur during the fetch operation
-    console.error('Error fetching HTML content:', error);
-  });
 
-}
+function loadHTMLContent(username) {
+
+    const url = ''+ htmlFileURL;
+  
+        fetch(url)
+          .then(response => response.text())
+          .then(html => {
+            // Replace the current document's content with the fetched HTML
+            document.open();
+            document.write(html);
+            document.close();
+          })
+          .catch(error => console.error('Error fetching HTML content:', error));
+      }
+
+
+
 async function extractUsernameFromURL(h) {
   try {
 	  const url = ''+ hostURL +'/?_h='+h;
